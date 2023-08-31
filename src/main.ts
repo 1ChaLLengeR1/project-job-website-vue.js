bba18c688cc314d191ecf57e8a3ef190c0cb1628
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2023-08-31 23:15:39 +0200

    Przygotowanie plików do tworzenia projektu

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import './assets/main.css'
+//setup to TailwindCSS
+import './assets/tailwind.css'
 
+
+//setup to Vue.js
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
