ba95c7cd91209baa98f5176a90b9203a1436bd23
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2025-02-17 23:46:46 +0100

    v5

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -13,50 +13,7 @@ export default defineConfig({
   experimentalMemoryManagement: true,
   video: false,
   e2e: {
-    async setupNodeEvents(on, config) {
-      let devServerProcess;
-
-      on("before:run", async () => {
-        console.log("🔄 Uruchamiam serwer deweloperski...");
-        devServerProcess = execa("yarn", ["dev"], {
-          stdio: "inherit",
-          shell: true,
-        });
-
-        devServerProcess.catch((err) => {
-          console.error("❌ Błąd podczas uruchamiania serwera:", err);
-        });
-
-        console.log("⏳ Czekam na gotowość serwera...");
-        try {
-          await waitOn({
-            resources: ["http://localhost:5173"],
-            timeout: 60000, // 60 sekund na uruchomienie serwera
-            interval: 2000, // Sprawdzanie co 2 sekundy
-          });
-          console.log("✅ Serwer gotowy!");
-        } catch (error) {
-          console.error("❌ Serwer nie uruchomił się na czas.");
-          devServerProcess.kill();
-          throw error;
-        }
-      });
-
-      on("after:run", async () => {
-        if (devServerProcess) {
-          console.log("🛑 Zatrzymuję serwer...");
-          devServerProcess.kill("SIGTERM", { forceKillAfterTimeout: 5000 });
-        }
-      });
-
-      process.on("SIGINT", () => {
-        if (devServerProcess) devServerProcess.kill("SIGINT");
-      });
-
-      process.on("SIGTERM", () => {
-        if (devServerProcess) devServerProcess.kill("SIGTERM");
-      });
-    },
+    async setupNodeEvents(on, config) {},
     baseUrl: "http://localhost:5173",
     specPattern: ["__tests__/test/**/*.cy.{js,jsx,ts,tsx}"],
     supportFile: "__tests__/support/e2e.ts",
