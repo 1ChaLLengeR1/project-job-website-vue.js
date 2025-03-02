c34525dbd93da881c39863c41babd84cf95bed57
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2025-03-02 21:27:58 +0100

    better install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,ts}"],
   theme: {
     extend: {
       colors: {
@@ -14,6 +14,9 @@ module.exports = {
         syne: ["Syne"],
       },
     },
+  },
+  corePlugins: {
+    preflight: false,
   },
   plugins: [],
 };
