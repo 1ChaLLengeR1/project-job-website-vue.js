c829a42c49f80d8e87bf91216a379781228e7370
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2025-07-02 11:21:34 +0200

    - add better deployment files

--- scripts/swarm_deploy.sh
+++ scripts/swarm_deploy.sh
@@ -2,30 +2,22 @@
 
 set -e
 
-APP_VERSION=$1
-
-if [ -z "$APP_VERSION" ]; then
-  echo "Podaj wersję jako argument. Przykład: ./swarm_deploy_frontend.sh 1.0.1"
-  exit 1
-fi
-
 LOG_FILE="swarm_deploy_frontend.log"
 DOCKER_USER="arturscibor"
 DOCKER_REPO="praca.strona.arturscibor.pl"
-FULL_IMAGE_NAME="$DOCKER_USER/$DOCKER_REPO:$APP_VERSION"
+FULL_IMAGE_NAME="$DOCKER_USER/$DOCKER_REPO:latest"
 
 echo ">>> Pobieranie obrazu z Docker Huba: $FULL_IMAGE_NAME ..."
 docker pull $FULL_IMAGE_NAME
-docker tag $FULL_IMAGE_NAME $DOCKER_USER/$DOCKER_REPO:latest
 
 echo ">>> Deploy stacka do Swarma..."
-docker stack deploy -c prod.swarm.docker-compose.yaml spinetime_frontend
+docker stack deploy -c ../prod.swarm.docker-compose.yaml spinetime_frontend
 
 echo ">>> Usuwanie nieużywanych obrazów..."
 docker image prune -f
 
 DEPLOY_DATE=$(date '+%Y-%m-%d %H:%M:%S')
-DEPLOY_MESSAGE="✅ Frontend Swarm deploy zakończony. Wersja: $APP_VERSION, Data: $DEPLOY_DATE"
+DEPLOY_MESSAGE="✅ Frontend Swarm deploy zakończony. Wersja: latest, Data: $DEPLOY_DATE"
 
 echo ">>> $DEPLOY_MESSAGE"
 echo "$DEPLOY_MESSAGE" >> "$LOG_FILE"
